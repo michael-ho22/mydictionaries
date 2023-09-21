24a554852b80e8eecdd9a21f319e62407ca6cfa7
Michael Ho <Michael_ Ho1@baylor.edu>
2023-09-21 00:56:56 -0500

    Homework Update (9/20)

---
+++ FileEncryption.py
@@ -0,0 +1,48 @@
+import csv
+
+codes = {
+    'A': '%', 'a': '9',
+    'B': '@', 'b': '#',
+    'C': '&', 'c': '5',
+    'D': '$', 'd': '1',
+    'E': '*', 'e': '8',
+    'F': '!', 'f': '7',
+    'G': '^', 'g': '6',
+    'H': '-', 'h': '3',
+    'I': '+', 'i': '2',
+    'J': '=', 'j': '0',
+    'K': ':', 'k': '/',
+    'L': ';', 'l': '|',
+    'M': '(', 'm': ')',
+    'N': '[', 'n': ']',
+    'O': '{', 'o': '}',
+    'P': '<', 'p': '>',
+    'Q': '.', 'q': ',',
+    'R': '?', 'r': '_',
+    'S': ' ', 's': '~',
+    'T': '`', 't': '\'',
+    'U': '"', 'u': '\\',
+    'V': '!', 'v': '&',
+    'W': '%', 'w': ':',
+    'X': '?', 'x': '@',
+    'Y': '|', 'y': '$',
+    'Z': '^', 'z': '`'
+}
+
+infile = open('info_security.txt', 'r')
+
+encrypted = open('encrypted.txt', 'w')
+
+text = infile.read()
+
+new_txt = ''
+for word in text:
+    if word in codes:
+        new_txt += codes[word]
+    else:
+        new_txt += word
+
+encrypted.write(new_txt)
+
+encrypted.close()
+
